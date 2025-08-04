3245fccab7d8b883cc55d23fa04faa51c1ff7aa1
gigapidor <nasarspektrreas@gmail.com>
2025-08-04 08:01:34 +0300

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const bookingRoutes = require('./routes/bookingRoutes');
+const bookingRoutes = require('./bookingRoutes');
 
 const app = express();
 
