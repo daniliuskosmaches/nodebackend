9fc4783c0d57c7bfdf84fa04cf626cd711e90b07
gigapidor <nasarspektrreas@gmail.com>
2025-08-04 08:02:11 +0300

    Update app.js

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use((err, req, res, next) => {
   res.status(500).send('Something broke!');
 });
 
-const PORT = process.env.PORT || 443;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
