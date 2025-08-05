a03baa4bd7c201ec1dd19a7d03ff0e0f0c996cf6
gigapidor <nasarspektrreas@gmail.com>
2025-08-05 19:08:53 +0300

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const path = require('path');
-const bookingRoutes = require('./backend/bookingRoutes');
+const bookingRoutes = require('./bookingRoutes');
 
 const app = express();
 
