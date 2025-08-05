7ef571a66eddd6b1dcdc1f27ee5ea3a7e832dfc5
gigapidor <nasarspektrreas@gmail.com>
2025-08-05 11:56:02 +0300

    Update bookingRoutes.js

--- bookingRoutes.js
+++ bookingRoutes.js
@@ -1,7 +1,16 @@
+
 const express = require('express');
 const bookingController = require('./bookingController');
 
 const router = express.Router();
+
+// Добавляем OPTIONS для предварительных CORS-запросов
+router.options('/', (req, res) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'POST, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  res.status(200).end();
+});
 
 router.post('/', bookingController.createBooking);
 
